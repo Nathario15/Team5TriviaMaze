4bb4d97b0cb36456d7719564046d2bcc830f5a5e
Ibrahim <ibrahim.elnikety@gmail.com>
2025-02-08 17:15:42 -0800

    Added Difficluty also, made changes to maze getting the room

---
+++ src/model/Difficulty.java
@@ -0,0 +1,16 @@
+package model;
+
+public enum Difficulty {
+	/**
+	 * This question is very hard.
+	 */
+	Hard,
+	/**
+	 * This question is of medium diffculty.
+	 */
+	Medium,
+	/**
+	 * This question is easy.
+	 */
+	Easy;
+}
--- src/model/Maze.java
+++ src/model/Maze.java
@@ -16,33 +16,62 @@ public final class Maze {
 	private Maze() {
 		
 	}
+//	/**
+//	 * Returns room North of player.
+//	 * @return
+//	 */
+//	public static Room getNorth() {
+//		return MAP[x][y + 1];
+//	}
+//	/**
+//	 * Returns room South of player.
+//	 * @return
+//	 */
+//	public static Room getSouth() {
+//		return MAP[x][y - 1];
+//	}
+//	/**
+//	 * Returns room East of player.
+//	 * @return
+//	 */
+//	public static Room getEast() {
+//		return MAP[x + 1][y];
+//	}
+//	/**
+//	 * Returns room West of player.
+//	 * @return
+//	 */
+//	public static Room getWest() {
+//		return MAP[x - 1][y];
+//	}
 	/**
-	 * Returns room North of player.
+	 * Returns room based on direction.
+	 * @param theDirection
 	 * @return
 	 */
-	public static Room getNorth() {
-		return MAP[x][y + 1];
+	public static Room getRoom(final Direction theDirection) {
+		Room temp = null;
+		if (theDirection == Direction.North) {
+			temp = MAP[x][y + 1];
+		}
+		if (theDirection == Direction.South) {
+			temp = MAP[x][y - 1];
+		}
+		if (theDirection == Direction.East) {
+			temp = MAP[x + 1][y];
+		}
+		if (theDirection == Direction.West) {
+			temp = MAP[x + 1][y];
+		}
+		return temp;
 	}
 	/**
-	 * Returns room South of player.
+	 * Returns room.
+	 * @param theDirection
 	 * @return
 	 */
-	public static Room getSouth() {
-		return MAP[x][y - 1];
-	}
-	/**
-	 * Returns room East of player.
-	 * @return
-	 */
-	public static Room getEast() {
-		return MAP[x + 1][y];
-	}
-	/**
-	 * Returns room West of player.
-	 * @return
-	 */
-	public static Room getWest() {
-		return MAP[x + 1][y];
+	public static Room getRoom() {
+		return MAP[x][y];
 	}
 	/**
 	 * moves and returns new room.
