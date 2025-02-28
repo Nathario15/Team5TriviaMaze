c924c2890f699dec49b738bcd1823465b7bcc223
Jay <jkp117@uw.edu>
2025-02-27 16:40:54 -0800

    Uncommented out a line in the constructor that can be used now.

--- src/view/GameView.java
+++ src/view/GameView.java
@@ -37,7 +37,7 @@ public class GameView extends JFrame {
 		myCardLayout = new CardLayout();
 		myMainPanel = new JPanel(myCardLayout);
 
-		// AddMainMenu();
+	    addMainMenu();
 		addGamePanel();
 		addInstructionsPanel();
 		// AddAboutPanel();
