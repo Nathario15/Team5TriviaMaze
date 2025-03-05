a1e23e631d7c45aab52b51af3e6c22f48f9e5bbc
Jay <jkp117@uw.edu>
2025-03-04 16:29:16 -0800

    Refactored TrueFalseQuestionPanel to extend QuestionPanel.

--- src/view/TrueFalseQuestionPanel.java
+++ src/view/TrueFalseQuestionPanel.java
@@ -8,76 +8,59 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class TrueFalseQuestionPanel extends JPanel {
+public final class TrueFalseQuestionPanel extends QuestionPanel {
     private static final long serialVersionUID = 1L;
+    private JButton myTrueButton;
+    private JButton myFalseButton;
 
-    private TrueFalseQuestion myQuestion;
-    private GameState myGameState;
-    private JLabel myQuestionLabel;
-    private JRadioButton myTrueButton;
-    private JRadioButton myFalseButton;
-    private ButtonGroup myButtonGroup;
-    private JButton mySubmitButton;
-    private JLabel myFeedbackLabel;
-
-    /**
-     * Constructor for TrueFalseQuestionPanel.
-     *
-     * @param question  The true/false question.
-     * @param gameState The current game state.
-     */
     public TrueFalseQuestionPanel(TrueFalseQuestion question, GameState gameState) {
-        this.myQuestion = question;
-        this.myGameState = gameState;
-
-        setLayout(new GridLayout(5, 1, 10, 10));
-
-        // Question Label
-        myQuestionLabel = new JLabel("<html><b>Question:</b> " + question.myQuestion + "</html>");
-        myQuestionLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        add(myQuestionLabel);
-
-        // True/False Buttons
-        myTrueButton = new JRadioButton("True");
-        myFalseButton = new JRadioButton("False");
-        myButtonGroup = new ButtonGroup();
-        myButtonGroup.add(myTrueButton);
-        myButtonGroup.add(myFalseButton);
-
-        add(myTrueButton);
-        add(myFalseButton);
-
-        // Submit Button
-        mySubmitButton = new JButton("Submit Answer");
-        mySubmitButton.addActionListener(new SubmitAnswerListener());
-        add(mySubmitButton);
-
-        // Feedback Label
-        myFeedbackLabel = new JLabel("");
-        myFeedbackLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        myFeedbackLabel.setForeground(Color.RED);
-        add(myFeedbackLabel);
+        super(question, gameState);
+        createAnswerInput();
     }
 
-    /**
-     * Inner class to handle answer submission.
-     */
-    private class SubmitAnswerListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            if (myTrueButton.isSelected() || myFalseButton.isSelected()) {
-                boolean selectedAnswer = myTrueButton.isSelected(); // True if selected, otherwise false
-                if (myQuestion.isCorrect(selectedAnswer)) {
-                    myFeedbackLabel.setText("Correct! You can proceed.");
-                    myFeedbackLabel.setForeground(Color.GREEN);
-                    myGameState.useQuestion(myQuestion.hashCode()); // Mark question as answered
-                } else {
-                    myFeedbackLabel.setText("Incorrect! Try again.");
-                    myFeedbackLabel.setForeground(Color.RED);
-                }
-            } else {
-                myFeedbackLabel.setText("Please select an answer.");
+    @Override
+    protected void createAnswerInput() {
+        // Set up the layout for True/False buttons
+        JPanel answerPanel = new JPanel();
+        answerPanel.setLayout(new GridLayout(1, 2));
+
+        myTrueButton = new JButton("True");
+        myFalseButton = new JButton("False");
+
+        myTrueButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkAnswer();
             }
+        });
+
+        myFalseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkAnswer();
+            }
+        });
+
+        answerPanel.add(myTrueButton);
+        answerPanel.add(myFalseButton);
+        add(answerPanel);
+    }
+
+    @Override
+    protected boolean checkAnswer() {
+        // Check if the selected answer is correct
+        String answer = myTrueButton.getModel().isPressed() ? "True" : "False";
+        boolean isCorrect = myQuestion.isCorrect(answer);
+
+        // Provide feedback to the user
+        if (isCorrect) {
+            myFeedbackLabel.setText("Correct! You can proceed.");
+            myFeedbackLabel.setForeground(Color.GREEN);
+        } else {
+            myFeedbackLabel.setText("Incorrect! Try again.");
+            myFeedbackLabel.setForeground(Color.RED);
         }
+
+        return isCorrect;
     }
 }
