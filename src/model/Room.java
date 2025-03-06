4d798be74a3722b3f7f5662903a4c01a6f1cdcd5
Nathaniel Roy <56801848+Nathario15@users.noreply.github.com>
2025-03-06 14:36:47 -0800

    Update Room.java
    Enhanced the class so that it better handles question associations and door states

--- src/model/Room.java
+++ src/model/Room.java
@@ -17,32 +17,49 @@ public class Room implements Serializable {
 	 * Shows which doors are open.
 	 */
 	protected final EnumMap<Direction, DoorState> myDoors = new EnumMap<>(Direction.class);
-//	/**
-//	 * Door to the north.
-//	 */
-//	private Boolean myNorthDoor = false;
-//	/**
-//	 * Door to the south.
-//	 */
-//	private Boolean mySouthDoor = false;
-//	/**
-//	 * Door to the east.
-//	 */
-//	private Boolean myEastDoor = false;
-//	/**
-//	 * Door to the west.
-//	 */
-//	private Boolean myWestDoor = false;
-
+    /**
+     * Questions associated with each door.
+     */
+    protected final EnumMap<Direction, AbstractQuestion> myQuestions = new EnumMap<>(Direction.class);
+    /**
+     * Room identifier.
+     */
+    private final int myRoomId;
+    /**
+     * Whether this room has been visited.
+     */
+    private boolean myIsVisited;
+    /**
+     * Whether this room is the exit.
+     */
+    private boolean myIsExit;
+    /**
+     * Whether this room is the entrance.
+     */
+    private boolean myIsEntrance;
 	/**
 	 * Creates a room object.
 	 */
 	public Room() {
+        this(0); // Call constructor with default ID
+	}
+	
+    /**
+     * Creates a room with a specific ID.
+     * 
+     * @param theRoomId The room's identifier
+     */
+    public Room(final int theRoomId) {
+        myRoomId = 0; // Default ID
+        myIsVisited = false;
+        myIsExit = false;
+        myIsEntrance = false;
+        
 		myDoors.put(Direction.NORTH, DoorState.Locked);
 		myDoors.put(Direction.SOUTH, DoorState.Locked);
 		myDoors.put(Direction.EAST, DoorState.Locked);
 		myDoors.put(Direction.WEST, DoorState.Locked);
-	}
+    }
 
 	/**
 	 * When player enters a room.
@@ -79,4 +96,107 @@ public class Room implements Serializable {
 		myDoors.put(theDir, DoorState.Blocked);
 		Maze.getRoom(theDir).blocked(theDir);
 	}
+	
+    /**
+     * Sets a question for a specific door direction.
+     * 
+     * @param theDirection The direction of the door
+     * @param theQuestion The question to associate with the door
+     */
+    public void setQuestion(final Direction theDirection, final AbstractQuestion theQuestion) {
+        myQuestions.put(theDirection, theQuestion);
+    }
+    
+    /**
+     * Gets the question for a specific door direction.
+     * 
+     * @param theDirection The direction of the door
+     * @return The question associated with the door, or null if none
+     */
+    public AbstractQuestion getQuestion(final Direction theDirection) {
+        return myQuestions.get(theDirection);
+    }
+    
+    /**
+     * Checks if a door has an associated question.
+     * 
+     * @param theDirection The direction of the door
+     * @return true if the door has a question, false otherwise
+     */
+    public boolean hasQuestion(final Direction theDirection) {
+        return myQuestions.containsKey(theDirection);
+    }
+    
+    /**
+     * Sets the visited status of the room.
+     * 
+     * @param theVisited Whether the room has been visited
+     */
+    public void setVisited(final boolean theVisited) {
+        myIsVisited = theVisited;
+    }
+    
+    /**
+     * Checks if the room has been visited.
+     * 
+     * @return true if the room has been visited, false otherwise
+     */
+    public boolean isVisited() {
+        return myIsVisited;
+    }
+    
+    /**
+     * Sets whether this room is the exit.
+     * 
+     * @param theIsExit Whether this room is the exit
+     */
+    public void setExit(final boolean theIsExit) {
+        myIsExit = theIsExit;
+    }
+    
+    /**
+     * Checks if this room is the exit.
+     * 
+     * @return true if this room is the exit, false otherwise
+     */
+    public boolean isExit() {
+        return myIsExit;
+    }
+    
+    /**
+     * Sets whether this room is the entrance.
+     * 
+     * @param theIsEntrance Whether this room is the entrance
+     */
+    public void setEntrance(final boolean theIsEntrance) {
+        myIsEntrance = theIsEntrance;
+    }
+    
+    /**
+     * Checks if this room is the entrance.
+     * 
+     * @return true if this room is the entrance, false otherwise
+     */
+    public boolean isEntrance() {
+        return myIsEntrance;
+    }
+    
+    /**
+     * Gets the room identifier.
+     * 
+     * @return The room identifier
+     */
+    public int getRoomId() {
+        return myRoomId;
+    }
+    
+    /**
+     * Gets the door state for a specific direction.
+     * 
+     * @param theDirection The direction to check
+     * @return The door state (Open, Locked, or Blocked)
+     */
+    public DoorState getDoorState(final Direction theDirection) {
+        return myDoors.get(theDirection);
+    }
 }
