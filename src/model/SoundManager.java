66afadeadfc4353f0f1faa5ee2a4ade5a03487eb
Jay <146704341+dukeron@users.noreply.github.com>
2025-03-20 14:00:10 -0700

    Fixed stopBackGroundMusic checkstyle.

--- src/model/SoundManager.java
+++ src/model/SoundManager.java
@@ -95,7 +95,6 @@ public final class SoundManager {
     public void stopBackgroundMusic() {
         if (myCurrentBackgroundMusic != null && myCurrentBackgroundMusic.isRunning()) {
             myCurrentBackgroundMusic.stop();
-        } else {
         }
     }
     
